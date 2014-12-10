6e95c7411788eee6299ac7817bc222f214d9dbfc
Paul Durrant <paul.durrant@citrix.com>
2014-12-10 16:21:15 +0000

    Revert script pollution that was accidentally pushed
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- msbuild.bat
+++ msbuild.bat
@@ -1,15 +1,7 @@
 call "%VS%\VC\vcvarsall.bat" x86
 @echo on
-
-cov-build --dir cov_dir msbuild.exe /m:1 /p:Configuration="%CONFIGURATION%" /p:Platform="%PLATFORM%" /t:"%TARGET%" %EXTRA% %FILE%
-
+msbuild.exe /m:1 /p:Configuration="%CONFIGURATION%" /p:Platform="%PLATFORM%" /t:"%TARGET%" %EXTRA% %FILE%
 if errorlevel 1 goto error
-
-cov-analyze --dir cov_dir -j auto --aggressiveness-level high --security --concurrency --preview --all --rule --enable-fnptr --strip-path c:\git\xenbus
-
-cov-commit-defects --dir cov_dir --host dagu-4.uk.xensource.com --port 8080 --user windows --password coverity --stream %PLATFORM%
-
-
 exit 0
 
 :error
