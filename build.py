49e35cf3a521a434fa543b3378b67ea300419a0e
Paul Durrant <paul.durrant@citrix.com>
2014-12-19 11:57:58 +0000

    Make sure version header gets re-built each time
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- build.py
+++ build.py
@@ -329,6 +329,8 @@ if __name__ == '__main__':
     sdv = { 'nosdv': False, None: True }
     driver = 'xenbus'
     vs = getVsVersion()
+
+    os.utime('include/version.hx', None)
     
     if 'COMPANY_NAME' not in os.environ.keys():
         os.environ['COMPANY_NAME'] = 'Xen Project'
