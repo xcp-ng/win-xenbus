f646f85788c7da813bb346085354b6aacd587571
Paul Durrant <paul.durrant@citrix.com>
2017-03-08 13:10:10 +0000

    Update XENBUS_EVTCHN interface with new Send method
    The new version of the method has the same arguments and return of the
    previous version but does not modify IRQL. It must therefore be called
    with IRQL >= DISPATCH_LEVEL. Most callers already do this (usually
    because they have a spin lock held) so the overhead of calling
    KeRaiseIrql() and KeLowerIrql() can be saved. Also, it makes the method
    safe to be called at > DISPATCH_LEVEL.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- include/revision.h
+++ include/revision.h
@@ -47,6 +47,7 @@
 #define DEFINE_REVISION_TABLE                                               \
     DEFINE_REVISION(0x08000009,  1,  2,  4,  1,  1,  1,  1,  1,  1,  1),    \
     DEFINE_REVISION(0x0800000A,  1,  2,  5,  1,  1,  1,  1,  1,  1,  1),    \
-    DEFINE_REVISION(0x0800000B,  1,  2,  5,  1,  2,  1,  1,  2,  1,  1)
+    DEFINE_REVISION(0x0800000B,  1,  2,  5,  1,  2,  1,  1,  2,  1,  1),    \
+    DEFINE_REVISION(0x0800000C,  1,  2,  6,  1,  2,  1,  1,  2,  1,  1)
 
 #endif  // _REVISION_H
