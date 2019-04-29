b5738b6d4b334fa1c3c23bfdce86cc6970087a93
Paul Durrant <paul.durrant@citrix.com>
2019-04-29 15:34:51 +0100

    Update Jenkins build script
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- ewdkbuild.bat
+++ ewdkbuild.bat
@@ -1,3 +1,5 @@
-call %EWDK%\BuildEnv\SetupBuildEnv.cmd
-powershell -Command .\build.ps1 free %SDV%
+call d:\BuildEnv\SetupBuildEnv.cmd
+set SDVBINPATH=%ToolsPath%\sdv\bin
+set PATH=%PATH%;%SDVBINPATH%
+powershell -Command .\build.ps1 free -sdv
 7z a -ttar xenbus.tar xenbus
