7e1245de1fb2dfc33266dc476433874f1e49e52d
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-06-06 01:33:51 +0200

    Repeat TryAutoReboot() with timer
    Add a timer that calls TryAutoReboot() every minute if reboot has been
    requested beforehand.
    
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- src/monitor/monitor.c
+++ src/monitor/monitor.c
@@ -57,6 +57,7 @@ typedef struct _MONITOR_CONTEXT {
     HANDLE                  EventLog;
     HANDLE                  StopEvent;
     HANDLE                  RequestEvent;
+    HANDLE                  Timer;
     HKEY                    RequestKey;
     PTCHAR                  Title;
     PTCHAR                  Text;
@@ -1347,6 +1348,7 @@ MonitorMain(
     PTCHAR              RequestKeyName;
     BOOL                Success;
     HRESULT             Error;
+    LARGE_INTEGER       DueTime;
 
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
@@ -1426,17 +1428,33 @@ MonitorMain(
     if (!Success)
         goto fail10;
 
+    Context->Timer = CreateWaitableTimer(NULL, FALSE, NULL);
+    if (Context->Timer == NULL)
+        goto fail11;
+
+    DueTime.QuadPart = -600000000; // 1 minute
+
+    Success = SetWaitableTimer(Context->Timer,
+                               &DueTime,
+                               60000,
+                               NULL,
+                               NULL,
+                               FALSE);
+    if (!Success)
+        goto fail12;
+
     SetEvent(Context->RequestEvent);
 
     ReportStatus(SERVICE_RUNNING, NO_ERROR, 0);
 
     for (;;) {
-        HANDLE  Events[3];
+        HANDLE  Events[4];
         DWORD   Object;
 
         Events[0] = Context->StopEvent;
         Events[1] = Context->RequestEvent;
         Events[2] = Context->ResponseEvent;
+        Events[3] = Context->Timer;
 
         Log("waiting (%u)...", ARRAYSIZE(Events));
         Object = WaitForMultipleObjects(ARRAYSIZE(Events),
@@ -1460,12 +1478,21 @@ MonitorMain(
                 DoReboot(NULL, 0);
             break;
 
+        case WAIT_OBJECT_0 + 3:
+            if (Context->RebootRequestedBy)
+                TryAutoReboot(Context->RebootRequestedBy);
+            break;
+
         default:
             break;
         }
     }
 
 done:
+    free(Context->RebootRequestedBy);
+    CancelWaitableTimer(Context->Timer);
+    CloseHandle(Context->Timer);
+
     (VOID) RegDeleteTree(Context->RequestKey, NULL);
 
     free(Context->Question);
@@ -1487,6 +1514,14 @@ done:
     Log("<====");
 
     return;
+
+fail12:
+    Log("fail12");
+
+    CloseHandle(Context->Timer);
+
+fail11:
+    Log("fail11");
 
 fail10:
     Log("fail10");
